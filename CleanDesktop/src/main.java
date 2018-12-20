056e81e7edeb971b5ec5a55ba6abf03965dd8ebc
Justin.Akdogan <Justin.Akdogan@AGHHZ-NB53-1.agiles.local>
2018-12-20 14:11:44 +0100

    Nothing

--- CleanDesktop/src/main.java
+++ CleanDesktop/src/main.java
@@ -1,9 +1,16 @@
+import javax.swing.JButton;
+
+import GUI.MainFrame;
 
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {	
+		MainFrame frame=new MainFrame();
+	    frame.setTitle("CleanDesktop");
+	   // frame.setResizable(false);
+	    frame.setVisible(true);
+	    //frame.setLayout(null);
+		//frame.setLocationRelativeTo(null);
 	}
 
 }
