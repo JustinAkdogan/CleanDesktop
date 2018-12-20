d3131a23c1b7a9cbf4dcde4ec979920d740777a5
Tim.Steinhagen <Tim.Steinhagen@AGHHZ-NB86-2.agiles.local>
2018-12-20 15:22:24 +0100

    new function in class ReadDesktop created

--- CleanDesktop/src/Functions/ReadDesktop.java
+++ CleanDesktop/src/Functions/ReadDesktop.java
@@ -1,5 +1,27 @@
 package Functions;
 
+import java.io.File;
+
 public class ReadDesktop {
 
+	String[] data; 
+	
+	String username = System.getProperty("user.name");
+	String path = "C:\\Users\\" +username+ "\\Desktop";
+	
+	public ReadDesktop() {
+		
+		reading();
+	}
+	
+	public void reading() {
+		
+		File desktopfile = new File(path);
+		String filelist[] = desktopfile.list();
+		int counter = filelist.length;
+		for(int i = 0; i <= counter - 1; i++)
+		System.out.println(filelist[i]);
+	}
+	
+	
 }
--- CleanDesktop/src/main.java
+++ CleanDesktop/src/main.java
@@ -1,6 +1,7 @@
 import javax.swing.JButton;
 
 import Functions.CreateFolders;
+import Functions.ReadDesktop;
 import GUI.MainFrame;
 
 public class main {
@@ -8,6 +9,7 @@ public class main {
 	public static void main(String[] args) {	
 		MainFrame frame=new MainFrame();
 		CreateFolders createfolders = new CreateFolders();
+		ReadDesktop rd = new ReadDesktop();
 	    frame.setTitle("CleanDesktop");
 	    frame.setResizable(false);
 	    frame.setVisible(true);
