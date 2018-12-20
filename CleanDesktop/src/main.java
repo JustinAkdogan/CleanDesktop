4bdc10f2d1f0046777433ea7d9974bbc729818e2
Tim.Steinhagen <Tim.Steinhagen@AGHHZ-NB86-2.agiles.local>
2018-12-20 14:12:35 +0100

    HAHA

--- CleanDesktop/src/main.java
+++ CleanDesktop/src/main.java
@@ -6,7 +6,7 @@ public class main {
 
 	public static void main(String[] args) {	
 		MainFrame frame=new MainFrame();
-	    frame.setTitle("CleanDesktop");
+	    frame.setTitle("CleanDesktop changed by ME");
 	   // frame.setResizable(false);
 	    frame.setVisible(true);
 	    //frame.setLayout(null);
