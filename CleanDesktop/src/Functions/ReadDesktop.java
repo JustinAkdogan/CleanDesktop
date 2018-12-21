6a2c9d0a1dc6b15e1ddbbd6badf8fd7773ebff9a
Johannes <Johannes@DESKTOP-AT8FVV6.fritz.box>
2018-12-21 20:55:53 +0100

    Bug Fixed

--- CleanDesktop/src/Functions/ReadDesktop.java
+++ CleanDesktop/src/Functions/ReadDesktop.java
@@ -15,24 +15,25 @@ public class ReadDesktop {
 	
 	public ReadDesktop(String prepath) {
 		path = prepath;
-		//reading();
-		createTransferLog();
-		createErrorLog();
+		reading();
+		//createTransferLog();
+		//createErrorLog();
 	}
 	
 	public void reading() {
-		
 		File desktopfile = new File(desktoppath);
 		String filelist[] = desktopfile.list();
 		int counter = filelist.length;
 		for(int i = 0; i <= counter - 1; i++) {			
 			File file = new File (desktoppath + filelist[i]);
-			long size = file.length();
-			if(size >= 1) {
-				new TransferFiles(filelist[i]); 
-			}else {
-				file.delete();
-			}
+			long size = filelist[i].length();
+			System.out.println("Name: " + file.getName() + "Size: " + size);
+			//if(size >= 1) {
+				//new TransferFiles(filelist[i]); 
+				//System.out.println("xD");
+			//}else {
+				//file.delete();
+			//}
 		}
 	}
 	
