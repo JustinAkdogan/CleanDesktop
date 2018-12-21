aeb47ff8289263adb28cdcaf3d21d60f0e0b3e5d
Justin.Akdogan <Justin.Akdogan@AGHHZ-NB53-1.agiles.local>
2018-12-21 13:41:20 +0100

    New Class "FileWhitelist"

--- CleanDesktop/src/Functions/FileWhitelist.java
+++ CleanDesktop/src/Functions/FileWhitelist.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 import javax.swing.JFileChooser;
@@ -14,6 +16,8 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 public class FileWhitelist {
 
 	String username = System.getProperty("user.name");
+	LocalDateTime now = LocalDateTime.now();
+
 	
 	public FileWhitelist() {
 		selectFiles();
@@ -36,9 +40,17 @@ public class FileWhitelist {
 	
 	public void saveFilesInWhitelist(File [] files) {
 		try {
-			BufferedWriter writer = new BufferedWriter (new FileWriter("C:\\CleanDesktop\\Whitelist.txt", true));
-			writer.write("d");
+			BufferedWriter writer = new BufferedWriter (new FileWriter("C:\\CleanDesktop\\Whitelist.txt", true));			
+			DateTimeFormatter df;
+			df = DateTimeFormatter.ofPattern("dd.MM.yyyy kk:mm");  
+			
+			//Text Writer
+			writer.write(""+now.format(df));
 			writer.newLine();
+			for (int i=0; i < files.length; i++) {
+				writer.write(""+files[i].getName());
+				writer.newLine();
+			}
 			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
